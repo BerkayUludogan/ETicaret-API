b539129f4c3cc9d2e3372c15cb70e63d5478fe19
Berkay Uludoğan <buludogan0@gmail.com>
2026-06-07 16:32:13 +0300

    Add project files.

---
+++ Presentation/ETicaret.API/Program.cs
@@ -0,0 +1,53 @@
+using ETicaret.API;
+
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHttpContextAccessor();//Client'tan gelen request neticesinde oluşturulan HttpContext nesnesine katmanlardaki class'lar üzerinden erişebilmemizi sağlar.
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+builder.Services.AppApi(builder.Configuration);
+
+
+
+builder.Services.AddControllers();
+#region Project Environments
+var env = builder.Environment;
+
+builder.Configuration
+    .SetBasePath(env.ContentRootPath)
+    .AddJsonFile("appsettings.json", optional: false)
+    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+#endregion
+
+
+
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseHttpsRedirection();
+
+app.UseRateLimiter(); // 1 dakikada maksimum 80 istek atılabilir. 80'den fazla istek atıldığında 429 Too Many Requests hatası dönecektir.
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ Presentation/ETicaret.API/ServiceRegistration.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.RateLimiting;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.RateLimiting;
+
+namespace ETicaret.API
+{
+    public static class ServiceRegistration
+    {
+        public static void AppApi(this IServiceCollection services, IConfiguration configuration)
+        {
+            #region Swagger
+            services.AddSwaggerGen(gen =>
+            {
+                var webUrl = "http://OnionArcTemp.com";
+                gen.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "OnionArcSharpCommerce Web Api",
+                    Version = "v1",
+                    License = new OpenApiLicense
+                    {
+                        Name = "Powered by Berkay Uludoğan",
+                        Url = new Uri(webUrl),
+                    },
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Berkay Uludoğan",
+                        Email = "buludogan0@gmail.com"
+                    }
+                });
+
+                gen.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization: Bearer {token}"
+                });
+                gen.AddSecurityRequirement(doc =>
+
+                    new OpenApiSecurityRequirement
+                    {
+                        [new OpenApiSecuritySchemeReference("Bearer", doc)] = new List<string>()
+                    });
+            });
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(opt =>
+                {
+                    opt.TokenValidationParameters = new()
+                    {
+                        ValidateAudience = true, // Oluşturulacak token değerinin kimlerin/hangi originlerin/sitelerin kullanıcı belirlediğimiz değerdir.
+                        ValidateIssuer = true, // Oluşturulacak token değerinin kimin dağıttığını ifade edeceğimiz alan
+                        ValidateLifetime = true, // Oluşturulan token değerinin süresini kontrol edecek olan doğrulama
+                        ValidateIssuerSigningKey = true, // Üretilecek token değerinin uygulamamıza ait bir değer olduğunu ifade eden suciry key verisinin doğrulamasıdır.
+
+                        ValidAudience = configuration["JWT:Audience"],
+                        ValidIssuer = configuration["JWT:Issuer"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:SecurityKey"])),
+                        LifetimeValidator = (notBefore, expires, securityToken, validationParameters) =>
+                        expires != null ? expires > DateTime.UtcNow : false,
+                        RoleClaimType = ClaimTypes.Role,
+                        NameClaimType = ClaimTypes.Name //JWT üzerinde Name claimne karşılık gelen değeri User.Identity.Name propertysinden elde edebiliriz.
+                    };
+                });
+
+            #endregion
+            #region Rate Limiting
+            services.AddRateLimiter(opt =>
+            {
+                opt.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+                opt.AddFixedWindowLimiter("fixed", limiterOpt =>
+                {
+                    limiterOpt.PermitLimit = 80; //80 Request
+                    limiterOpt.Window = TimeSpan.FromMinutes(1);
+                    limiterOpt.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+                    limiterOpt.QueueLimit = 0;
+                });
+            });
+            #endregion
+        }
+    }
+}
