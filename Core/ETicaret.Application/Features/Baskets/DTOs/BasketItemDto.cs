98aba5a75cd23f50dc507fa72bf24e73788d59fc
Berkay Uludoğan <buludogan0@gmail.com>
2026-06-17 22:03:53 +0300

    feat(basket): add get my basket endpoint

---
+++ Core/ETicaret.Application/Features/Baskets/DTOs/BasketItemDto.cs
@@ -0,0 +1,13 @@
+﻿namespace ETicaret.Application.Features.Baskets.DTOs
+{
+    public class BasketItemDto
+    {
+        public Guid Id { get; set; }
+        public Guid ProductId { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public string ProductSlug { get; set; } = string.Empty;
+        public decimal UnitPrice { get; set; }
+        public int Quantity { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
---
+++ Presentation/ETicaret.API/Controllers/BasketsController.cs
@@ -0,0 +1,36 @@
+﻿
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ETicaret.API.Attributes;
+using System.Security.Claims;
+using ETicaret.Application.Features.Baskets.Queries.GetMyBasket;
+namespace ETicaret.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public BasketsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [JwtAuthorize]
+        [HttpGet("my-basket")]
+        public async Task<IActionResult> GetMyBasket()
+        {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (string.IsNullOrWhiteSpace(userId))
+                return Unauthorized();
+
+            var response = await _mediator.Send(new GetMyBasketQueryRequest
+            {
+                UserId = Guid.Parse(userId)
+            });
+            return Ok(response);
+        }
+    }
+
+}
