5316b9e600aaf86d6e2d1d11423df3feaed6a074
Berkay Uludoğan <buludogan0@gmail.com>
2026-06-20 00:18:04 +0300

    feat(payment): add mock order payment

---
+++ Core/ETicaret.Application/Features/Payments/Commands/PayOrder/PayOrderCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace ETicaret.Application.Features.Payments.Commands.PayOrder
+{
+    public class PayOrderCommandValidator : AbstractValidator<PayOrderCommandRequest>
+    {
+        public PayOrderCommandValidator()
+        {
+            RuleFor(x => x.OrderId)
+               .NotEmpty();
+
+            RuleFor(x => x.PaymentMethod)
+                .IsInEnum();
+        }
+    }
+}
---
+++ Presentation/ETicaret.API/Controllers/PaymentsController.cs
@@ -0,0 +1,36 @@
+﻿using ETicaret.API.Attributes;
+using ETicaret.API.Extensions;
+using ETicaret.Application.Features.Payments.Commands.PayOrder;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaret.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public PaymentsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [JwtAuthorize]
+        [HttpPost("pay-order")]
+        public async Task<IActionResult> PayOrder(PayOrderCommandRequest request)
+        {
+            var userId = User.GetUserId();
+
+            if (userId is null)
+                return Unauthorized();
+
+            request.UserId = userId.Value;
+
+            var response = await _mediator.Send(request);
+
+            return Ok(response);
+        }
+    }
+}
